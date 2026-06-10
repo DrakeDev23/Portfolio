3da593257b50bb29507db1e07c4415c37eab05e4
DrakeDev23 <maccogoth@gmail.com>
2026-06-10 15:58:00 +0800

    updated folder structure

--- frontend/src/components/header.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function header(){
-    return(
-        <header className="bg-blue-500 text-center">
-            <h1 className="text-center">drake gwapo</h1>
-        </header>
-    )
-}
