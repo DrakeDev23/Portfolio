662b480a62b910243ec740a49f486b43b8c4fc0a
DrakeDev23 <maccogoth@gmail.com>
2026-08-17 22:12:45 +0800

    fix bug sa AI

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     EMAILJS_PUBLIC_KEY:  str
     EMAILJS_PRIVATE_KEY: str
     GROQ_API_KEY: str
-    GROQ_MODEL: str = "llama-3.3-70b-versatile"
+    GROQ_MODEL: str = "openai/gpt-oss-20b"
     DATABASE_URL: str
 
     class Config:
